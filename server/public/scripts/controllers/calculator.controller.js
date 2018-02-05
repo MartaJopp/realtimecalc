0abd3b21761bc4fd5038ff73fcc4c6f2b043f8d6
Marta Jopp <martapeterson@gmail.com>
2018-02-05 14:01:25 -0600

    basic calculator function

--- server/public/scripts/controllers/calculator.controller.js
+++ server/public/scripts/controllers/calculator.controller.js
@@ -2,4 +2,30 @@ myApp.controller('CalculatorController', function ($http, $scope, socket, Calcul
     var vm = this;
     vm.calculatorService = CalculatorService;
 
+//start display with 0
+vm.screen = "";
+
+vm.equation = {
+    problem: '',
+    
+}
+
+//updates the screen by adding onto the string
+vm.updateScreen = function (value) {
+    vm.screen += value;
+}
+
+vm.total = function (value) {
+    // vm.equation.problem = vm.screen;
+    // console.log(value)
+    vm.screen += value + eval(vm.screen);
+    vm.equation = vm.screen;
+    console.log(vm.equation);
+
+}
+
+
+
+    
+
 })//end controller
